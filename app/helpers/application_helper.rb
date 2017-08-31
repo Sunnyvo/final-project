2a73ee7be8a79dab4353edc26a9dde6a8f15ac33
Sunny <votanphu.2101@gmail.com>
2017-08-31 09:34:39 +0700

    first  commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,14 @@
+module ApplicationHelper
+  def icon(icon_name, text)
+    content_tag(:span, fa_icon(icon_name), class: "icon") + content_tag(:span, text)
+  end
+  def flash_messages(opts = {})
+    flash.map do |msg_type, message|
+      content_tag(:div, class: "message #{class_for(msg_type)}") do
+        content_tag(:div, class: "message-header") do
+          (message + content_tag(:button, 'x'.html_safe, class: 'delete')).html_safe
+        end
+      end
+    end.join.html_safe
+  end
+end
